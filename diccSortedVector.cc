d486afeaa472ccb6ae4bb7a6c8abd0368ff84f1f
Ricardo Lopez <104648450+RLPUPC@users.noreply.github.com>
2022-10-16 22:54:30 +0200

    Add files via upload

--- diccSortedVector.cc
+++ diccSortedVector.cc
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <string.h>
 #include "ED.cc"
+#include <algorithm>
 
 
 using namespace std;
@@ -17,65 +18,13 @@ private:
 
     vector<string> Dictionary;
 
-    /*
-    PRE:
-        a,b son de strings no vacios
-    POST:
-        El resultado es la comparación de dos palabras
-        si a > b = 1
-        si a = b = 0
-        si a < b = -1
-    */
-    int CompareStrings(string a, string b){
-        int va, vb; 
-        for(int i=0; i < a.size();++i){
-            va = 0;
-            vb = 0;
-            if(a[i] >= 'a' && a[i] <= 'z'){
-                va = a[i] - 'a';
-            }
-            else if(a[i] >= 'A' && a[i] <= 'Z'){
-                va = a[i] - 'A';
-            }
-            if(b[i] >= 'a' && b[i] <= 'z'){
-                vb = b[i] - 'a';
-            }
-            else if(b[i] >= 'A' && b[i] <= 'Z'){
-                vb = b[i] - 'A';
-            }
-            if(va > vb || b.size() <= i){
-                return 1;
-            }
-            if(va < vb)
-                return -1;
-        }
-        if(a.size() == b.size())
-            return 0;
-        return -1;
-    }
-
-    /*
-    Pre:
-        inf: valor minimo de la búsqueda
-        sup: valor máximo de la búsqueda
-        n: es la palabra a buscar
-    Post:
-        -1 si la palabra no esta en el diccionario
-        >=0 si la palabra esta en el diccionario
-    */
     SearchResult FindDicctionary(int inf, int sup, string n){
-        //cout << "valores " << inf << " " << sup << " " << n << " " << endl; 
         if(inf > sup)
         return SearchResult::WORD_NOT_FOUND;
         else {
             int central;
             central = (inf + sup)/2;
             int cpm = n.compare(Dictionary[central]);
-            /*cout << "central " << central << endl; 
-            cout << "comparar " << cpm << endl;
-            cout << "palabra central " << Dictionary[central] << endl;  
-            cout << "palabra inferior " << Dictionary[inf] << endl;  
-            cout << "palabra superior " << Dictionary[sup] << endl;*/
             if(cpm == 0){
                 return SearchResult::WORD_FOUND;
             }
@@ -90,29 +39,13 @@ private:
 
 public:
 
-    diccSortedVector() { 
-        Dictionary = vector<string>();
-    }
-
     void addWord(string word) {
+        if(word=="") return;
         Dictionary.push_back(word);
     }
 
-    void CreateDictionary(){
-        char cadena[128];
-        ifstream fe("./EjemploDic.txt");
-
-        while(!fe.eof()){
-            fe >> cadena;
-            Dictionary.push_back(cadena);
-        }
-    }
-
-    void WriteDicctionary(){
-        cout << "Escribimos diccionario" << endl;
-        for(int i = 0; i < Dictionary.size();++i){
-            cout << Dictionary[i] << endl;
-        }
+    void sorterDictionary(){
+        sort(Dictionary.begin(), Dictionary.end());
     }
 
     SearchResult findWord(string n){
